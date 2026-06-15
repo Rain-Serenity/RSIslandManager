e7a29a046de3c01d5b156828ab7036f0657dcdf7
Chosen_1st <Chosen_1st@Outlook.com>
2026-06-16 01:29:38 +0800

    5.0.2

--- src/main/java/com/rserene/chosen/server/rsislandmanager/Settings/DeepOceanGuardian.java
+++ src/main/java/com/rserene/chosen/server/rsislandmanager/Settings/DeepOceanGuardian.java
@@ -2,7 +2,6 @@ package com.rserene.chosen.server.rsislandmanager.Settings;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.block.Biome;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,24 +11,21 @@ import com.rserene.chosen.server.rsislandmanager.RSIslandManager;
 
 public class DeepOceanGuardian implements Listener {
 
-    private static final Biome TARGET_BIOME = Biome.DEEP_COLD_OCEAN;
-
     public DeepOceanGuardian() {
         Bukkit.getPluginManager().registerEvents(this, JavaPlugin.getPlugin(RSIslandManager.class));
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onNaturalSpawn(CreatureSpawnEvent event) {
-        if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) return;
+    public void onSpawn(CreatureSpawnEvent event) {
         EntityType type = event.getEntityType();
-        if (type != EntityType.SQUID && type != EntityType.COD
-            && type != EntityType.SALMON && type != EntityType.PUFFERFISH
-            && type != EntityType.TROPICAL_FISH) return;
+        if (type != EntityType.COD && type != EntityType.SALMON
+            && type != EntityType.PUFFERFISH && type != EntityType.TROPICAL_FISH
+            && type != EntityType.SQUID) return;
 
         Location loc = event.getLocation();
-        if (loc.getBlock().getBiome() != TARGET_BIOME) return;
+        if (!"deep_cold_ocean".equals(loc.getBlock().getBiome().getKey().getKey())) return;
 
         event.setCancelled(true);
-        loc.getWorld().spawnEntity(loc, EntityType.GUARDIAN, CreatureSpawnEvent.SpawnReason.NATURAL);
+        loc.getWorld().spawnEntity(loc, EntityType.GUARDIAN);
     }
 }
